c7354ebee98dc7dfca8476894d06f586ae236b3d
jeong-mi <wjdal6951@naver.com>
2022-04-28 01:06:05 +0900

    chore: 같은 import 버그해결

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useReducer, createContext } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-import * as Api from "./api"
 import { loginReducer } from "./reducer"
 
 import Header from "./components/Header"
@@ -9,18 +8,18 @@ import Footer from "./components/Footer"
 import MainPage from "./components/mainpage/MainPage.jsx"
 import Question from "./components/question/Question"
 import Login from "./components/user/Login"
-import Register from "./components/user/Register"
 import Signin from "./components/user/Signin"
 import Password from "./components/user/Password"
 import Mypage from "./components/mypage/Mypage"
 import Team from "./components/team/Team"
+import DataLog from "./components/datalogs/DataLog"
+import Result from "./components/result/Result"
 
 import { MainWrapper } from "./srcAssets/style/MainWrapper"
 import './srcAssets/style/Font.module.css'
 
 import { MuiThemeProvider, createTheme} from '@material-ui/core/styles';
-import DataLog from "./components/datalogs/DataLog"
-import Result from "./components/result/Result"
+
 
 export const UserStateContext = createContext(null)
 export const DispatchContext = createContext(null)
@@ -31,17 +30,12 @@ function App() {
     user: null,
   })
 
-  const themeMuiCore = createTheme({
-    typography: {
-      fontFamily: '"Elice Digital Baeum", sans-serif'
-    },
-  });
 
   return (
     <DispatchContext.Provider value={dispatch}>
       <UserStateContext.Provider value={userState}>
 
-        <MuiThemeProvider theme={themeMuiCore}>
+        
 
           <Router>
             <MainWrapper>
@@ -62,7 +56,7 @@ function App() {
             </MainWrapper>
           </Router>
 
-        </MuiThemeProvider>
+        
       </UserStateContext.Provider>
     </DispatchContext.Provider>
   )
