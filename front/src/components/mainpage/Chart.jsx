a10a6e59b2162ee47f10347369a5bd032cc2d72f
kimdahyeon <anniy8920@naver.com>
2022-04-30 22:38:55 +0900

    feat : local commit

--- front/src/components/mainpage/Chart.jsx
+++ front/src/components/mainpage/Chart.jsx
@@ -5,13 +5,14 @@ function Chart(){
     return (
       <ComposedChart width={730} height={250} data={data}>
         <XAxis dataKey="year" />
-        <YAxis type="number" domain={[3, 6]}/>
+        <YAxis type="number" domain={[3, 8]}/>
         <Tooltip />
         <Legend />
         <CartesianGrid stroke="#f5f5f5" />
         <Area type="monotone" dataKey="health" fill="#8884d8" stroke="#8884d8" />
         <Bar dataKey="happinessScore" barSize={20} fill="#413ea0" />
         <Line type="monotone" dataKey="socialSupport" stroke="#ff7300" />
+        <Line type="monotone" dataKey="gdp" stroke="#9933ff" />
       </ComposedChart>
     )
 }
