9edeccbc7747841ede8ecb188203e337cce04229
kimdahyeon <anniy8920@naver.com>
2022-04-30 23:52:22 +0900

    feat : Radialchart 오류수정

--- data/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-from os.path import dirname, join
-from dotenv import load_dotenv
-load_dotenv(verbose=True)
-dotenv_path=join(dirname(__file__),".env")
-PORT=os.environ.get("FLASK_PORT")
-DEBUG=os.environ.get("FLASK_DEBUG")
