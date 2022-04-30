ac82e64ca2af3956e8ca41c30c3c61baaa19e4eb
ParkSuJeong <sujeong9158@gmail.com>
2022-05-01 01:56:59 +0900

    chore: ranking 구현중

--- data/controller/happy_data.py
+++ data/controller/happy_data.py
@@ -2,17 +2,18 @@ import csv
 from pymongo import MongoClient
 
 
-with open ("../file/2022_google_csv.csv", "r") as f:
+with open ("../file/happy_data2.csv", "r") as f:
     reader = csv.reader(f)
     next(reader)
     data = {"Happiness": []}
     for row in reader:
-        data["Happiness"].append({"Rank":row[0],"country":row[1], "happinessScore":row[2],
+        data["Happiness"].append({"rank":row[0],"country":row[1], "happinessScore":row[2],
         "dytopia":row[3],"gdp":row[4],"socialSupport":row[5],"health":row[6],
-        "freedom":row[7],"generosity":row[8],"corruptionPerceptions":row[9], "count": 0 })
+        "freedom":row[7],"generosity":row[8],"corruptionPerceptions":row[9],  "continent":row[11], "count": row[14] })
 
 
 client = MongoClient("mongodb+srv://crackingdevs:elice1234@crackingdevs.guuao.mongodb.net/crackingdevs")
 for i in data["Happiness"]:
     db = client['crackingdevs']
+    # print(i)
     dpInsert = db.happinesses.insert_one(i)
