79cf5d0871ff794dd8ce4fa994aa8c2f1d718135
jeong-mi <wjdal6951@naver.com>
2022-05-02 20:52:56 +0900

    style: css 수정

--- front/src/components/question/Question.jsx
+++ front/src/components/question/Question.jsx
@@ -1,16 +1,24 @@
 import {  Container } from "@mui/material"
+import { useState } from "react"
+import AlertDialog from "./AlertDialog"
 import QuestionContent from "./QuestionContent"
 import QuestionHeader from "./QuestionHeader"
 
 function Question(){
+    const [modalOpen, setModalOpen] = useState(true);
 
     return (
+    <>
+        {modalOpen && 
+        <AlertDialog modalOpen={modalOpen} setModalOpen={setModalOpen} />}
+
         <Container sx={{py: 7, mt: 12}}>
 
-            <QuestionHeader></QuestionHeader>
+            {/* <QuestionHeader></QuestionHeader> */}
             <QuestionContent></QuestionContent>
 
         </Container>
+    </>
     )
 }
 export default Question
