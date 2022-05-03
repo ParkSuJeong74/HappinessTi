56594b13c50902558c15772c06c118c3d049d7c3
jeong-mi <wjdal6951@naver.com>
2022-05-03 16:52:47 +0900

    feat: scrollToTopButton

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,10 +1,42 @@
 import Style from 'styled-components'
+import { useEffect, useState } from 'react';
+import style from '../srcAssets/style/Mainpage.module.css'
+import { Button } from '@mui/material';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 function Footer(){
+    const [showButton, setShowButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.pageYOffset > 300) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        });
+    }, []);
+
+    // window창을 제일 위로 가게 하는 함수
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' 
+        });
+    };
+
     return (
+        <>
+        {showButton && (
+            <Button onClick={scrollToTop} className={style.backToTop}>
+                <ArrowUpwardIcon />
+            </Button>
+        )}
+        
         <FooterDiv>
             <p>Copyrightⓒ2022 by crashingdev. All Page content is property of 행복Ti</p>
         </FooterDiv>
+        </>
     )
 }
 
