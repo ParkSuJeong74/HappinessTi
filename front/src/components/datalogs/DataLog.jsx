d19c733056cee31a2603f106860b214bc9c0e82d
LoadWithWater <60064819+LoadWithWater@users.noreply.github.com>
2022-05-04 00:36:03 +0900

    Chore: 결과 페이지-데이터 수정

--- front/src/components/datalogs/DataLog.jsx
+++ front/src/components/datalogs/DataLog.jsx
@@ -2,26 +2,58 @@ import { Container } from "@mui/material"
 import { DataGrid } from '@mui/x-data-grid';
 import data from '../../srcAssets/style/Data.module.css'
 
+import * as Api from "../../api";
+import { useEffect, useState } from "react";
+
 const columns = [
-    { field: 'type', headerName: '유형', width: 160 },
-    { field: 'icon', headerName: 'Icon', width: 130 },
-    { field: 'happiness', headerName: '행복 지수', width: 130 },
-    { field: 'freedom', headerName: '자유 지수', width: 130 },
+    { field: 'rank', headerName: '랭킹', width: 130 },
+    { field: 'country', headerName: '국가', width: 130 },
+    { field: 'happinessScore', headerName: '행복 지수', width: 130 },
+    { field: 'dytopia', headerName: '자유 지수', width: 130 },
     { field: 'gdp', headerName: 'GDP 지수', width: 130 },
-    { field: 'trust', headerName: '정부 신뢰 지수', width: 130 },
+    { field: 'socialSupport', headerName: '정부 신뢰 지수', width: 130 },
     { field: 'health', headerName: '건강 지수', width: 130 },
+    { field: 'freedom', headerName: 'Rank', width: 130 },
+    { field: 'generosity', headerName: 'Country', width: 130 },
+    { field: 'corruptionPerceptions', headerName: 'Distopia', width: 130 },
+    { field: 'continent', headerName: 'contient', width: 130 },
+    { field: 'count', headerName: 'Count', width: 130 },
 ];
 
 // userlog api 호출받기
-const rows = [
-    { id: 1, type: 'Norway 형', icon: 'Jon', happiness: 75, freedom: 95, gdp: 323, trust: 509, health: 903 },
-    { id: 2, type: 'Denmark 형', icon: 'Jon', happiness: 34, freedom: 955, gdp: 835, trust: 53, health: 950 },
-    { id: 3, type: 'Korea 형', icon: 'Jon', happiness: 53, freedom: 34, gdp: 65, trust: 189, health: 354 },
-    { id: 4, type: 'Denmark 형', icon: 'Jon', happiness: 34, freedom: 955, gdp: 835, trust: 53, health: 950 },
-    { id: 5, type: 'Korea 형', icon: 'Jon', happiness: 53, freedom: 34, gdp: 65, trust: 189, health: 354 }
-];
+// const rows = [
+//     { id: 1, type: 'Norway 형', icon: 'Jon', happiness: 75, freedom: 95, gdp: 323, trust: 509, health: 903 },
+//     { id: 2, type: 'Denmark 형', icon: 'Jon', happiness: 34, freedom: 955, gdp: 835, trust: 53, health: 950 },
+//     { id: 3, type: 'Korea 형', icon: 'Jon', happiness: 53, freedom: 34, gdp: 65, trust: 189, health: 354 },
+//     { id: 4, type: 'Denmark 형', icon: 'Jon', happiness: 34, freedom: 955, gdp: 835, trust: 53, health: 950 },
+//     { id: 5, type: 'Korea 형', icon: 'Jon', happiness: 53, freedom: 34, gdp: 65, trust: 189, health: 354 }
+// ];
 
 function DataLog(){
+    const [datas, setDatas] = useState([])
+    const [rows, setRows] = useState([])
+
+    async function getResultData() {
+        try {
+          const res = await Api.get("happiness/lists");
+          const originalData = res.data;
+
+          for(let i=0; i< originalData.length; i++){
+            originalData[i]['id'] = i+1;
+          }
+          console.log(res)
+          console.log(res.data)
+
+          setRows(originalData)
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+    useEffect(() => {
+        getResultData()
+    }, [])
+
     return (
         <Container sx={{py: 7, mt: 12}}>
             <div className={data.dataTitle1}>
@@ -33,7 +65,7 @@ function DataLog(){
 
             <div className={data.dataHappyTi} style={{ height: 381, width: '85%', margin: '0 auto'}}>
                 <DataGrid
-                    onRowClick={(e) => console.log(e.row.type.split(" ")[0])}
+                    //onRowClick={(e) => console.log(e.row.type.split(" ")[0])}
                     rows={rows}
                     columns={columns}
                     pageSize={5}
