1e462b1e7aee462e4d938cb423a45b4cbf3d94b8
jeong-mi <wjdal6951@naver.com>
2022-05-04 20:25:10 +0900

    chore: composedChart범위변경, 선 색상변경, treemap 대륙이름 자세히

--- data/controller/chartcode.py
+++ data/controller/chartcode.py
@@ -1,18 +1,23 @@
+#-- coding: utf-8 --
 from flask import Flask, jsonify, Blueprint
 from sklearn.preprocessing import MinMaxScaler
 import json
 import pandas as pd
 import numpy as np
-
 df = pd.read_csv('./file/happy_data2.csv')
 df_merged = pd.read_csv('./file/df_merged.csv')
 cc = Blueprint('cc',__name__)
-scaler=MinMaxScaler()
 ##----treemap.png------##
 @cc.route('/tree',methods=['GET'])
 def treemap():
   tree_data=[]
   children=[]
+  df['continent']=df['continent'].replace('EU','Europe')
+  df['continent']=df['continent'].replace('AS','Asia')
+  df['continent']=df['continent'].replace('OC','Oceania')
+  df['continent']=df['continent'].replace('NA','North America')
+  df['continent']=df['continent'].replace('AF','Africa')
+  df['continent']=df['continent'].replace('SA','South Africa')
   for j in df['continent'].unique():
     test2={
       'name':j,
@@ -30,82 +35,6 @@ def treemap():
     tree_data.append(result2)
   return jsonify(tree_data)
 
-#barplot10-1.png#
-#왼쪽부터 오른쪽 순서#
-@cc.route('/gdp/bar',methods=['GET'])
-def gdp_barplot():
-  gdp_data=[]
-  for i in range(0,len(df.nlargest(10,'gdp'))):
-      test={
-        'country':df.nlargest(10,'gdp')['country'].to_list()[i],
-        'happinessScore':df.nlargest(10,'gdp')['happinessScore'].to_list()[i],
-      }
-      result=eval(json.dumps(test))
-      gdp_data.append(result)
-  return jsonify(gdp_data)
-
-@cc.route('/social/bar',methods=['GET'])
-def social_barplot():
-  social_data=[]
-  for i in range(0,len(df.nlargest(10,'socialSupport'))):
-      test={
-        'country':df.nlargest(10,'socialSupport')['country'].to_list()[i],
-        'happinessScore':df.nlargest(10,'socialSupport')['happinessScore'].to_list()[i],
-      }
-      result=eval(json.dumps(test))
-      social_data.append(result)
-  return jsonify(social_data)
-
-@cc.route('/health/bar',methods=['GET'])
-def health_barplot():
-  health_data=[]
-  for i in range(0,len(df.nlargest(10,'health'))):
-      test={
-        'country':df.nlargest(10,'health')['country'].to_list()[i],
-        'happinessScore':df.nlargest(10,'health')['happinessScore'].to_list()[i],
-      }
-      result=eval(json.dumps(test))
-      health_data.append(result)
-  return jsonify(health_data)
-
-@cc.route('/freedom/bar',methods=['GET'])
-def freedom_barplot():
-  freedom_data=[]
-  for i in range(0,len(df.nlargest(10,'freedom'))):
-      test={
-        'country':df.nlargest(10,'freedom')['country'].to_list()[i],
-        'happinessScore':df.nlargest(10,'freedom')['happinessScore'].to_list()[i],
-      }
-      result=eval(json.dumps(test))
-      freedom_data.append(result)
-  return jsonify(freedom_data)
-
-##barplot10-2.png##
-@cc.route('/generosity/bar',methods=['GET'])
-def generosity_barplot():
-  generosity_data=[]
-  for i in range(0,len(df.nlargest(10,'generosity'))):
-      test={
-        'country':df.nlargest(10,'generosity')['country'].to_list()[i],
-        'happinessScore':df.nlargest(10,'generosity')['happinessScore'].to_list()[i],
-      }
-      result=eval(json.dumps(test))
-      generosity_data.append(result)
-  return jsonify(generosity_data)
-
-@cc.route('/corruption/bar',methods=['GET'])
-def corruption_barplot():
-  corruptionPerceptions_data=[]
-  for i in range(0,len(df.nlargest(10,'corruptionPerceptions'))):
-      test={
-        'country':df.nlargest(10,'corruptionPerceptions')['country'].to_list()[i],
-        'happinessScore':df.nlargest(10,'corruptionPerceptions')['happinessScore'].to_list()[i],
-      }
-      result=eval(json.dumps(test))
-      corruptionPerceptions_data.append(result)
-  return jsonify(corruptionPerceptions_data)
-
-
 ##----맵차트 ------##
 @cc.route('/mapplot',methods=['GET'])
 def mapplot():
@@ -118,12 +47,6 @@ def mapplot():
       result=eval(json.dumps(test))
       map_data.append(result)
   return jsonify(map_data)
-
-##----대륙별로 시각화----##
-##찾아보니 nivo에서 zoom을 조절할 수 있는것같습니다##
-##projectionScale로 확대해주시고
-##projection Translation으로 위치를 조정해주셔서 대륙별로 보여주세요!!#
-
 
 ##---score per continent.png----#
 # Grouping data on basis of continents 🐾
@@ -147,7 +70,7 @@ def continent_barplot():
 #--------군집분석------#
 @cc.route('/similar',methods=['GET'])
 def similar():
-  dict={'1':['India', 'Tanzania', 'Zimbabwe'],
+  dict={1:['India', 'Tanzania', 'Zimbabwe'],
         '2':['Finland', 'Denmark', 'Iceland', 'Switzerland', 'Netherlands', 'Luxembourg',
  'Sweden', 'Norway', 'Israel'],
         '3':['Cambodia', 'Uganda', 'Nigeria', 'Kenya', 'Pakistan', 'Madagascar', 'Ethiopia',
@@ -206,46 +129,53 @@ def radar(country):
           "fill": "#8884d8",
       }
       test2={
-          'name':'dystopia',
+          'name':'디스토피아',
           'uv':temp['dystopia'].to_list()[0],
           "fill":"#83a6ed",
       }
       test3={
-          'name':'socialSupport',
+          'name':'사회적지지',
           'uv':temp['socialSupport'].to_list()[0],
           "fill": "#8dd1e1",
       }
       test4={
-          'name':'health',
+          'name':'건강',
           'uv':temp['health'].to_list()[0],
           "fill": "#82ca9d",
       }
       test5={
-          'name':'freedom',
+          'name':'자유',
           'uv':temp['freedom'].to_list()[0],
           "fill": "#a4de6c",
       }
       test6={
-          'name':'generosity',
+          'name':'관대함',
           'uv':temp['generosity'].to_list()[0],
           "fill": "#d0ed57",
       }
       test7={
-          'name':'corruptionPerceptions',
+          'name':'부패인식',
           'uv':temp['corruptionPerceptions'].to_list()[0],
           "fill": "#ffc658",
       }
-      data.append(eval(json.dumps(test1)))
-      data.append(eval(json.dumps(test2)))
-      data.append(eval(json.dumps(test3)))
-      data.append(eval(json.dumps(test4)))
-      data.append(eval(json.dumps(test5)))
-      data.append(eval(json.dumps(test6)))
-      data.append(eval(json.dumps(test7)))
+      for j in range(1,8):
+        data.append(eval(json.dumps(locals()['test{}'.format(j)])))
       test[i]=data
       dic.append(test)
-  dic[0].get(country).sort(key=lambda x: x.get('uv'),reverse=True)
+  dic[0].get(country).sort(key=lambda x: x.get('uv'),reverse=False)
   return jsonify(dic[0].get(country))
+
+
+##------결과페이지-------------##
+@cc.route('/text/<country>',methods=['GET'])
+def result(country):
+  temp2=df[df['country']==country]
+  for i in ['dystopia','gdp','socialSupport','health','freedom','generosity','corruptionPerceptions']:
+    temp2['rank_{}'.format(i)] = df[i].rank(method='dense', ascending=False)
+    globals()['{}_per'.format(i)]=round(temp2['rank_{}'.format(i)].to_list()[0]/df.shape[0]*100,3)
+    globals()['{}_text'.format(i)]= '높' if globals()['{}_per'.format(i)]<=50 else '낮'
+  rank=round(temp2['RANK'].values[0]/df.shape[0]*100)
+  return jsonify({'rank': rank, "gdpPer":[gdp_per,gdp_text], 'dystopiaPer':[dystopia_per,dystopia_text], 'socialPer':[socialSupport_per,socialSupport_text],'healthPer':[health_per,health_text],'freedomPer':[freedom_per,freedom_text],'generosityPer':[generosity_per,generosity_text],'corruptionPer':[corruptionPerceptions_per,corruptionPerceptions_text]})
 
 #----composed barchart----------#
 @cc.route('/composed',methods=['GET'])
@@ -258,13 +188,9 @@ def composedBarchart():
     test={
         'year':i,
         "happinessScore":list(yearAvg[yearAvg.index==i]['Happiness Score'].values)[0],
-        "socialSupport":list(yearAvg[yearAvg.index==i]['Family (Social Support)'].values)[0],
-        'gdp':list(yearAvg[yearAvg.index==i]['Economy (GDP per Capita)'].values)[0],
-        'health':list(yearAvg[yearAvg.index==i]['Health (Life Expectancy)'].values)[0],
+        "socialSupport":list(yearAvg[yearAvg.index==i]['Family (Social Support)'].values*4)[0],
+        'gdp':list(yearAvg[yearAvg.index==i]['Economy (GDP per Capita)'].values*4)[0],
+        'health':list(yearAvg[yearAvg.index==i]['Health (Life Expectancy)'].values*8)[0],
     }
     data.append(eval(json.dumps(test)))
   return jsonify(data)
-    
-    
-
-    
--- front/src/components/mainpage/chart/ChartComposed.jsx
+++ front/src/components/mainpage/chart/ChartComposed.jsx
@@ -1,8 +1,6 @@
 import React, {useEffect, useState} from "react"
 import { ComposedChart, XAxis,  YAxis, Tooltip, Legend, CartesianGrid, Area, Bar, Line} from 'recharts';
 import * as Api from '../../../api'
-
-
 function ChartComposed({active}){
   const [data, setData] = useState(null)
   useEffect(()=>{
@@ -15,19 +13,20 @@ function ChartComposed({active}){
     }
   },[])
   
-  return (
-    <ComposedChart width={730} height={250} data={data}>
-      <XAxis dataKey="year" />
-      <YAxis type="number" domain={[3, 7]}/>
-      <Tooltip />
-      <Legend />
-      <CartesianGrid stroke="#f5f5f5" />
-      <Area isAnimationActive={active} type="monotone" dataKey="health" fill="#8884d8" stroke="#8884d8" />
-      <Bar isAnimationActive={active} dataKey="happinessScore" barSize={20} fill="#413ea0" />
-      <Line isAnimationActive={active} type="monotone" dataKey="socialSupport" stroke="#ff7300" />
-      <Line type="monotone" dataKey="gdp" stroke="#9933ff" />
-    </ComposedChart>
-  )
+  
+    return (
+      <ComposedChart width={730} height={250} data={data}>
+        <XAxis dataKey="year" />
+        <YAxis type="number" domain={[3, 7]}/>
+        <Tooltip />
+        <Legend />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Area isAnimationActive={active} type="monotone" dataKey="health" fill="#8884d8" stroke="#8884d8" />
+        <Bar isAnimationActive={active} dataKey="happinessScore" barSize={20} fill="#413ea0" />
+        <Line isAnimationActive={active} type="monotone" dataKey="socialSupport" stroke="#ff66ff" />
+        <Line type="monotone" dataKey="gdp" stroke="#9900cc" />
+      </ComposedChart>
+    )
 }
 
 export default ChartComposed
