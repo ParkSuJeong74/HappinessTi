0a778d974d2e0d2835a36a60a804d0ddad5fba47
jeong-mi <wjdal6951@naver.com>
2022-05-05 20:53:43 +0900

    feat: mypage 데이터 가져올 동안 loading.. 띄우기

--- front/src/components/Mypage/Mypage.js
+++ front/src/components/Mypage/Mypage.js
@@ -8,13 +8,18 @@ import {ROUTES} from '../../Route'
 import ProfileInfo from "./ProfileInfo";
 import Profile from "./Profile";
 import errorHandler from "../../errorHandler";
+import Style from '../../srcAssets/style/Mypage.module.css'
+import loading from '../../srcAssets/img/loading.gif'
 
 function Mypage() {
   const navigate = useNavigate();
 
+  const isLoggedin = sessionStorage.getItem("userToken");
   const [user, setUser] = useState(null);
   const [surveyLog, setSurveyLog] = useState([]);
   const [editOpen, setEditOpen] = useState(false);
+
+  const [isFetchCompleted, setIsFetchCompleted] = useState(false);
 
   const toggleEditForm = () => {
     setEditOpen((prev) => !prev)
@@ -23,8 +28,6 @@ function Mypage() {
   const updateUser = (user) => {
     setUser(user);
   };
-
-  const isLoggedin = sessionStorage.getItem("userToken");
 
   // user정보 호출하기
   async function getUserData() {
@@ -54,18 +57,29 @@ function Mypage() {
       errorHandler("사용자 로그 정보 불러오기 오류", err.response.data)
       console.log(err.response.data);
     }
+    setIsFetchCompleted(true)
   }
 
   useEffect(() => {
     
     getUserData();
     getUserLogs()
+    
 
     if (!isLoggedin) {
       alert("반가워요! 먼저 로그인을 해주세요!");
       navigate(ROUTES.LOGIN.link, { replace: true });
     }
   }, []);
+
+  // 로딩될 동안에 기다리는 중 gif 띄워놓기?
+  if (!isFetchCompleted) {
+    return <div className={Style.loading}>
+      <img src={loading} alt="로딩중.." className={Style.loadingImg}/>
+      <h1>데이터 불러오는 중입니다...</h1>
+    </div>
+  }
+  
 
   return (
     <Container sx={{ py: 7, mt: 12 }}>
