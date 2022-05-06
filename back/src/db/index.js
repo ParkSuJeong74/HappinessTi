3f97ade898ff97e9435381fc3b0d095b66eaeee2
ParkSuJeong <sujeong9158@gmail.com>
2022-05-07 02:42:04 +0900

    re

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -19,10 +19,10 @@ mongoose.connect(DB_URL);
 const db = mongoose.connection;
 
 db.on("connected", () =>
-  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
+  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  ")
 );
 db.on("error", (error) =>
-  console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
+  console.error("MongoDB 연결에 실패하였습니다..." + "\n" + error)
 );
 
 export { userModel, happyModel, surveylogModel, resultModel };
